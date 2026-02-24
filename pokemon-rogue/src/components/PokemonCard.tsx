31284b3e86a8c071a140cdf30fc0f39e83d1e2a5
Quoc Bao Dinh Le <q.le102100@gmail.com>
2026-02-24 16:47:11 -0500

    Add PokemonCard component
    Create a reusable card component to display pokemon stats.
    It accepts a Pokemon object as a prop and renders name,
    hp, maxHp, attack, and speed using Tailwind styling.

---
+++ pokemon-rogue/src/components/PokemonCard.tsx
@@ -0,0 +1,16 @@
+import { Pokemon } from '../types/pokemon';
+
+interface PokemonCardProps {
+    pokemon: Pokemon;
+}
+
+export const PokemonCard = ({pokemon} : PokemonCardProps) => {
+    return (
+        <div className='border-2 border-slate-800 p-2 rounded-xl bg-slate-100 w-50'>
+            <p className='text-xl font-bold'>{pokemon.name}</p>
+            <p>HP: {pokemon.hp}/{pokemon.maxHp}</p>
+            <p>Attack: {pokemon.attack}</p>
+            <p>Speed: {pokemon.speed}</p>
+        </div>
+    )
+}
